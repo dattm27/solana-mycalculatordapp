ad5d9795d8b8ef907902dd84a03a44eabce096ef
dattm27 <dattran2003.ttn@gmail.com>
2025-09-13 13:30:47 +0700

    feat: add other operators

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -19,6 +19,27 @@ pub mod mycalculatordapp {
     calculator.result = num1 + num2; 
     Ok(())
    }
+
+
+   pub fn sub(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult{
+    let  calculator = &mut ctx.accounts.calculator;
+    calculator.result = num1 - num2;
+    Ok(()) 
+   }
+
+   pub fn multiply(ctx: Context<Multiplication>, num1 : i64, num2: i64) -> ProgramResult{
+    let calculator = &mut ctx.accounts.calculator;
+    calculator.result = num1 *num2;
+    Ok(())
+   }
+
+
+   pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+    let calculator = &mut ctx.accounts.calculator;
+    calculator.result = num1/ num2;
+    calculator.remainder = num1 % num2;
+    Ok(())
+   }
 }
 
 
@@ -37,9 +58,28 @@ pub struct Addition<'info>{
     pub calculator: Account<'info, Calculator>
 }
 
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+} 
+
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub  calculator: Account<'info, Calculator>
+}
 
 #[account] 
 pub struct Calculator {
     pub greeting: String,
     pub result: i64,
+    pub remainder: i64
 }
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -50,5 +50,39 @@ describe("mycalculatordapp", async () => {
     assert.ok(account.result.eq(new anchor.BN(5)))
   })
 
+  it("Subtract numbers", async () => {
+    await program.rpc.sub(new anchor.BN(5), new anchor.BN(2), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(3)));
+  })
+
+  it("Multiply two numbers", async () => {
+    await program.rpc.multiply(new anchor.BN(2), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    });
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(6)));
+  })
+
+  it("Divide two numbers", async () => {
+    await program.rpc.divide(new anchor.BN(5), new anchor.BN(2), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(2)));
+    assert.ok(account.remainder.eq(new anchor.BN(1)));
+  })
+
 })
 
